91e4a6a954a2081ad06b254feae5387bcb7696b2
allexmagno <allex.m@aluno.ifsc.edu.br>
2020-05-29 16:32:45 -0300

    removendo arquivos desnecessarios

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ docker run -d -p 80:80 -p 443:443 --name shib-test \
 --volume $PWD/src/shibboleth/attribute-map.xml:/etc/shibboleth/attribute-map.xml \
 --volume $PWD/src/apache2/shib_ssl.conf:/etc/apache2/sites-available/default-ssl.conf \
 --volume $PWD/log/apache2:/var/log/apache2 \
+--volume $PWD/log/shibboleth:/var/log/shibboleth/ \
 -i -t shib_test:1.0
